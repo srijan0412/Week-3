d2e38df6684439af2dd83169f566f805a06f9020
srijan0412 <srijan.soni01@gmail.com>
2025-01-31 21:58:44 +0530

    InsertionSort

---
+++ Day3/InsertionSort.java
@@ -0,0 +1,34 @@
+public class InsertionSort {
+    // Method to sort an integer array with Insertion sort algorithm
+    public static int[] sortArray(int array[]) {
+        int arraySize = array.length;
+
+        for (int i = 1; i < arraySize; i++) {
+            int key = array[i];
+            int j = i - 1;
+            // Move elements that are greater than key to one position ahead
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+
+
+        return array;
+    }
+
+    public static void main(String[] args) {
+        // Defining a sample array which stores the employee id of 5 employees
+        int employeeIdArray[] = {102, 101, 105, 103, 104}; //Employee ID of 5 employees 
+
+        // Sorting the array by InsertionSort
+        int sortedMarksArray[] = sortArray(employeeIdArray);
+
+        // Output 
+        System.out.println("The sorted marks array is: ");
+        for (int element : sortedMarksArray) {
+            System.out.print(element + ", ");
+        }
+    }
+}
