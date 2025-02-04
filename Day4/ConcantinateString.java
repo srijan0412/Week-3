c9664d3173754237b525b63beb48bf6be07c9b1d
srijan0412 <srijan.soni01@gmail.com>
2025-02-04 11:00:08 +0530

    Concatinate String

---
+++ Day4/ConcantinateString.java
@@ -0,0 +1,29 @@
+//Concatinate string using string buffer 
+public class ConcantinateString {
+    public static String concatString(String string1, String string2) {
+        // Creating a string a buffer object 
+        StringBuffer sb = new StringBuffer();
+
+        // Iterating over the strings and appending it to the string buffer 
+        for (int i = 0; i < string1.length(); i++) {
+            sb.append(string1.charAt(i));
+        }
+
+        for (int i = 0; i < string2.length(); i++) {
+            sb.append(string2.charAt(i));
+        }
+
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        // Creating a sample string to test the concatination function 
+        String sampleString1 = "Hello ";
+        String sampleString2 = "world!";
+
+        // Calling the concatString function 
+        String result = concatString(sampleString1, sampleString2);
+
+        // Output 
+        System.out.println("The concatinated string is : " + result);
+    }
+}
