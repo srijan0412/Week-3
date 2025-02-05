2a3f54cb5de7d84dee267b6ef070f20fd15a64d7
srijan0412 <srijan.soni01@gmail.com>
2025-02-05 11:36:08 +0530

    [Srijan] Add . Program to Search word in a Array of sentences

---
+++ Day5/Linear Search/SearchWordInSentences.java
@@ -0,0 +1,30 @@
+public class SearchWordInSentences {
+    // Method to find a word in an array of sentences 
+    public static boolean findWordInSentences(String[] sentences, String word){
+        for (int i = 0; i < sentences.length; i++) {
+            String sentence[] = sentences[i].split("//s+");
+            for (int j = 0; j < sentence.length; j++) { 
+                if (sentence[j] == word) {
+                    return true;
+                }
+            }
+        }
+        return false; // word not found case 
+    }
+    public static void main(String[] args) {
+        // Defining a sample array of sentences for testing 
+        String sentences[] = {"This is sentence1", "This is sentence2", "This is sentence3"};
+        String keyWord = "sentence2";
+
+        // Calling the findWordInSentences() function 
+        boolean result = findWordInSentences(sentences, keyWord);
+
+        // Output 
+        if (result) {
+            System.out.println("The word exists in the sentences");
+        }
+        else {
+            System.out.println("The word does not exist.");
+        }
+    }
+}
