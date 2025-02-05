8ef45cbc525f95b7d1bb8811b19f37815c906e6d
srijan0412 <srijan.soni01@gmail.com>
2025-02-05 12:33:26 +0530

    [Srijan] Fix . Fixing previous solved problem

--- Day5/Linear Search/SearchWordInSentences.java
+++ Day5/Linear Search/SearchWordInSentences.java
@@ -2,15 +2,16 @@ public class SearchWordInSentences {
     // Method to find a word in an array of sentences 
     public static boolean findWordInSentences(String[] sentences, String word){
         for (int i = 0; i < sentences.length; i++) {
-            String sentence[] = sentences[i].split("//s+");
+            String sentence[] = sentences[i].split(" ");
             for (int j = 0; j < sentence.length; j++) { 
-                if (sentence[j] == word) {
+                if (word.equals(sentence[j])) {
                     return true;
                 }
             }
         }
         return false; // word not found case 
     }
+
     public static void main(String[] args) {
         // Defining a sample array of sentences for testing 
         String sentences[] = {"This is sentence1", "This is sentence2", "This is sentence3"};
@@ -24,7 +25,7 @@ public class SearchWordInSentences {
             System.out.println("The word exists in the sentences");
         }
         else {
-            System.out.println("The word does not exist.");
+            System.out.println("The word does not exist in the sentences.");
         }
     }
 }
